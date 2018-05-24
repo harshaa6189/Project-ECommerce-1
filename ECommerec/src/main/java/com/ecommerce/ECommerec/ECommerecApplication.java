91910bde04fb53256abe7614247a1f2ce21a4dca
Achint Mistry <achint@Achints-MacBook-Pro.local>
2018-05-24 13:34:50 +0530

    project intialization

---
+++ ECommerec/src/main/java/com/ecommerce/ECommerec/ECommerecApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ECommerec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerecApplication.class, args);
+	}
+}
